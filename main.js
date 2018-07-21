5cc1c9e823e4e509df00661fe13085e69f1af53c
johanplee <johanplee@gmail.com>
2018-07-21 16:47:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 
+function init() {
+  Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1AVijnBgZpuVLi70VZhdoZpTzUalbNjINQZeOl1O2tbo/edit?usp=sharing',
+                   callback: function(data, tabletop) { 
+                       console.log(data)
+                   },
+                   simpleSheet: true } )
+}
+window.addEventListener('DOMContentLoaded', init)
